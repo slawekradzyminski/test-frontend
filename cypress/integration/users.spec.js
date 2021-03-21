ea51df2b117883ee6bf2855a976c7e4ad55b1dfc
Slawek <s.radzyminski@ocado.com>
2021-03-21 11:39:57 +0100

    localstorage

--- cypress/integration/users.spec.js
+++ cypress/integration/users.spec.js
@@ -4,12 +4,14 @@
 context('Actions', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080')
-    cy.setLocalStorage("cookies-accepted", "true")
-    cy.reload()
+    const user = { token: '12345' };
+    window.localStorage.setItem('user', JSON.stringify({ user }))
+    cy.visit('http://localhost:8080')
   })
 
   it('should login', () => {
-    cy.get('ul li').last().contains(`${randomFirstName} ${randomLastName}`)
+    cy.intercept('GET', '**/users', { fixture: 'allusers.json' })
+    cy.get('ul li').first().contains(`Slawomir Radzyminski`)
   })
 
 })
