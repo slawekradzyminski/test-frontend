9b22dae193d7dba0463bf4850e2c73357a1b1b3e
Slawek <s.radzyminski@ocado.com>
2021-04-09 19:51:27 +0200

    cy.request example

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -5,10 +5,27 @@ context('With backend enabled - login', () => {
     cy.visit('http://localhost:8080/login')
   })
 
-  it('should login', () => {
+  it('should login via UI', () => {
     cy.get('[name=username]').type('slawenty')
     cy.get('[name=password]').type('password')
     cy.get('button').click()
+    cy.get('.congrats').first().should('have.text', "You're logged in! Congratulations :)")
+    cy.get('ul li').should('have.length', 2)
+    // cy.get('.delete').first().click()
+    // cy.get('ul li').should('have.length', 1)
+  })
+
+  it('should login via request', () => {
+    cy.request({
+      method:'POST',
+      url: 'http://localhost:4000/users/authenticate',
+      body: {username: "gosianowak123", password: "password"}
+      })
+      .then((resp) =>{
+          console.log(resp.body)
+          window.localStorage.setItem('user',JSON.stringify(resp.body))
+      })
+    cy.visit('http://localhost:8080')
     cy.get('.congrats').first().should('have.text', "You're logged in! Congratulations :)")
     cy.get('ul li').should('have.length', 2)
     // cy.get('.delete').first().click()
