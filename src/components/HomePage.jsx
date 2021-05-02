6803e68280b48f1b173174d2d81de7a8d74b934e
Slawek <s.radzyminski@ocado.com>
2021-05-02 17:24:07 +0200

    refactor HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -15,14 +15,35 @@ function HomePage() {
         dispatch(userActions.getAll());
     }, []);
 
-    function handleDeleteUser(username) {
-        dispatch(userActions.delete(username));
-    }
+    const handleDeleteUser = username => dispatch(userActions.delete(username))
 
     const editUser = (user) => {
         localStorage.setItem("userToEdit", JSON.stringify(user));
         dispatch(userActions.saveEditDetails(user));
         history.push('/edit-user');
+    }
+
+    const displayDeleteSection = user => {
+        const displayLoading = () => <em> - Deleting...</em>
+        const displayDeleteError = () => <span className="text-danger"> - ERROR: {user.deleteError}</span>
+        const handleDeleteClick = () => window.confirm("Are you sure you wish to delete this item?") && handleDeleteUser(user.username)
+        const displayDeleteButton = () => <span> - <a onClick={handleDeleteClick} className="text-primary delete">Delete</a></span>
+
+        return user.deleting ? displayLoading()
+            : user.deleteError ? displayDeleteError()
+                : displayDeleteButton();
+    }
+
+    const displayUsers = () => {
+        return <ul>
+            {users.items.map((user) =>
+                <li key={user.id}>
+                    {`${user.firstName} ${user.lastName}`}
+                    {isAdmin && displayDeleteSection(user)}
+                    {<span> - <a onClick={() => editUser(user)} className="text-primary edit">Edit</a></span>}
+                </li>
+            )}
+        </ul>;
     }
 
     return (
@@ -32,26 +53,7 @@ function HomePage() {
             <h3>All registered users:</h3>
             {users.loading && <em>Loading users...</em>}
             {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-            {users.items &&
-            <ul>
-                {users.items.map((user) =>
-                    <li key={user.id}>
-                        {user.firstName + ' ' + user.lastName}
-                        {isAdmin && (
-                            user.deleting ? <em> - Deleting...</em>
-                                : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
-                                : <span> - <a onClick={() =>
-                                    window.confirm("Are you sure you wish to delete this item?") &&
-                                    handleDeleteUser(user.username)
-                                } className="text-primary delete">Delete</a></span>)
-                        }
-                        {
-                            <span> - <a onClick={() => editUser(user)} className="text-primary edit">Edit</a></span>
-                        }
-                    </li>
-                )}
-            </ul>
-            }
+            {users.items && displayUsers()}
             <p>
                 <Link id="logout" to="/login">Logout</Link>
             </p>
