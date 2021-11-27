a873fd18707c512614b79ed455ccd89f8be1f40b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-11-27 10:06:52 +0100

    day2-2

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,9 +2,22 @@
 
 describe('login page', () => {
     beforeEach(() => {
+        cy.visit('http://localhost:8080')
     })
   
     it('should successfully login', () => {
+        cy.get('[name=username]').type('slawenty')
+        cy.get('[name=password]').type('password')
+        cy.get('button').click()
+        cy.get('h1').should('have.text', 'Hi Slawomir!')
+        cy.get('#app p').first().should('contain.text', 'Congratulations')
+    })
+
+    it('should show error failed message', () => {
+        cy.get('[name=username]').type('wrong')
+        cy.get('[name=password]').type('wrong')
+        cy.get('button').click()
+        cy.get('.alert-danger').should('have.text', 'Login failed - bad username or password')
     })
   
   })
