5343cde49402b3daf5fb88e6047fc1664274e9a5
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-11-28 16:25:38 +0100

    day 3.9

--- cypress/integration/mocks/mocked.edit.spec.js
+++ cypress/integration/mocks/mocked.edit.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+const { getRandomString } = require('../../util/random')
+
 describe('Edit page with mocks', () => {
 
     const users = require('../../fixtures/users.json')
@@ -7,16 +9,56 @@ describe('Edit page with mocks', () => {
     beforeEach(() => {
         cy.setTokenInLocalStorage()
         cy.intercept('GET', '**/users', { fixture: 'users.json' })
+        cy.intercept('GET', `**/users/${users[2].id}`, { fixture: 'gosiakowalska.json' })
         cy.visit('')
     })
 
     it('should correctly autocomplete fields', () => {
         // when
-        cy.intercept('GET', `**/users/${users[2].id}`, { fixture: 'gosiakowalska.json'})
         cy.get('ul li').eq(2).find('.edit').click()
-        
+
         // then
         verifyUserDataAutocompleted(users[2])
+    })
+
+    it('should edit user', () => {
+        // given
+        const newFirstName = getRandomString()
+        const newLastName = getRandomString()
+        const newUsername = getRandomString()
+        const newPassword = getRandomString()
+
+        cy.get('ul li').eq(2).find('.edit').click()
+        cy.get("[name='firstName']").clear().type(newFirstName)
+        cy.get("[name='lastName']").clear().type(newLastName)
+        cy.get("[name='username']").clear().type(newUsername)
+        cy.get("[name='password']").clear().type(newPassword)
+
+        const id = users[2].id
+        cy.intercept('PUT', `**/users/${id}`, {
+            statusCode: 200,
+            body: {
+                id: id,
+                firstName: newFirstName,
+                lastName: newLastName,
+                username: newUsername,
+                password: newPassword,
+            }
+        }).as('putRequest')
+
+        // when
+        cy.get('button').click()
+
+        // then
+        cy.wait('@putRequest').its('request.body')
+            .should('deep.equal', {
+                id: id,
+                firstName: newFirstName,
+                lastName: newLastName,
+                username: newUsername,
+                password: newPassword,
+            })
+
     })
 
 })
