1088b449023e5337ab0c0b319e4c43567b6ba04b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-12-11 16:14:33 +0100

    day 2.11

---
+++ cypress/integration/edit.spec.js
@@ -0,0 +1,50 @@
+/// <reference types="cypress" />
+
+import { getRandomString } from "../util/random"
+
+describe('Edit page', () => {
+
+    const username = getRandomString()
+    const password = getRandomString()
+    const firstName = getRandomString()
+    const lastName = getRandomString()
+    let userId
+
+    beforeEach(() => {
+        cy.register(username, password, firstName, lastName)
+        .then(id => userId = id)
+        cy.login(username, password)
+    })
+
+    afterEach(() => {
+        cy.deleteUser(userId)
+    })
+
+    it('should edit user', () => {
+        // given
+        const newFirstName = getRandomString()
+        const newLastName = getRandomString()
+        cy.get('ul li').contains(`${firstName} ${lastName}`).find('.edit').click()
+        cy.get('[name=firstName]').clear().type(newFirstName)
+        cy.get('[name=lastName]').clear().type(newLastName)
+
+        // when
+        cy.get('.btn-primary').click()
+
+        // then
+        cy.get('ul li').contains(`${newFirstName} ${newLastName}`).should('be.visible')
+        cy.get('ul li').contains(`${firstName} ${lastName}`).should('not.exist')
+    })
+
+    it('should fill in correct data', () => {
+        // when
+        cy.get('ul li').contains(`${firstName} ${lastName}`).find('.edit').click()
+
+        // then
+        cy.get('[name=firstName]').should('have.value', firstName)
+        cy.get('[name=lastName]').should('have.value', lastName)
+        cy.get('[name=username]').should('have.value', username)
+        cy.get('[name=password]').should('have.value', password)
+    })
+
+})
