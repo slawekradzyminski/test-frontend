e036bcd94cc3565e25e17b626a248468d657159b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2021-12-11 12:14:12 +0100

    day 2.6

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,15 @@
 Cypress.Commands.add('login', (username, password) => {
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:4000/users/authenticate',
+        body: {
+            username: username,
+            password: password
+        }
+    }).then(resp => {
+        cy.wrap(resp.status).should('eq', 200)
+        expect(resp.status).to.eq(200)
+        window.localStorage.setItem('user', JSON.stringify(resp.body))
+    })
     cy.visit('')
-    cy.get('[name=username]').type(username)
-    cy.get('[name=password]').type(password)
-    cy.get('.btn-primary').click()
 })
