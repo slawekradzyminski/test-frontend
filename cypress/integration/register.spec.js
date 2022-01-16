e9fc1808cadae090e42d4297fecc04e6d5070360
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-01-16 14:11:07 +0100

    36

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -18,28 +18,18 @@ describe('register page', () => {
     })
 
     it('should fail to register if username already exists', () => {
-        cy.get('[name=firstName]').type(getRandomString())
-        cy.get('[name=lastName]').type(getRandomString())
-        cy.get('[name=username]').type('slawenty')
-        cy.get('[name=password]').type(getRandomString())
-        cy.get('button').click()
-
-        cy.get('.alert-danger').should('contain.text', 'User already exists')
+        registerPage.register('slawenty', getRandomString(), getRandomString(), getRandomString())
+        registerPage.verifyUserAlreadyExistsError()
     })
 
     it('cancel button should navigate back to login page', () => {
-        cy.get('.btn-link').click()
+        registerPage.clickCancel()
         cy.url().should('contain', 'login')
     })
 
     it('frontend validation should work', () => {
-        cy.get('button').click()
-        cy.get('.invalid-feedback').eq(0).should('contain.text', 'First Name is required')        
-        cy.get('.invalid-feedback').eq(1).should('contain.text', 'Last Name is required')        
-        cy.get('.invalid-feedback').eq(2).should('contain.text', 'Username is required')        
-        cy.get('.invalid-feedback').eq(3).should('contain.text', 'Password is required')  
-        cy.get('.is-invalid').should('have.length', 4)
+        registerPage.clickRegister()
+        registerPage.verifyFrontendValidation()
     })
-
 
   })
--- cypress/pages/registerPage.js
+++ cypress/pages/registerPage.js
@@ -5,15 +5,39 @@ class RegisterPage {
     usernameField = '[name=username]'
     passwordField = '[name=password]'
     registerButton = 'button'
+    alertError = '.alert-danger'
+    cancelLink = '.btn-link'
+    registerButton = 'button'
+    invalidFeedback = '.invalid-feedback'
+    invalidInput  = '.is-invalid'
 
     register(username, password, firstName, lastName) {
         cy.get(this.firstNameField).type(firstName)
         cy.get(this.lastNameField).type(lastName)
         cy.get(this.usernameField).type(username)
         cy.get(this.passwordField).type(password)
+        this.clickRegister()
+    }
+
+    verifyUserAlreadyExistsError() {
+        cy.get(this.alertError).should('contain.text', 'User already exists')
+    }
+
+    clickCancel() {
+        cy.get(this.cancelLink).click()
+    }
+
+    clickRegister() {
         cy.get(this.registerButton).click()
     }
 
+    verifyFrontendValidation() {
+        cy.get(this.invalidFeedback).eq(0).should('contain.text', 'First Name is required')        
+        cy.get(this.invalidFeedback).eq(1).should('contain.text', 'Last Name is required')        
+        cy.get(this.invalidFeedback).eq(2).should('contain.text', 'Username is required')        
+        cy.get(this.invalidFeedback).eq(3).should('contain.text', 'Password is required')  
+        cy.get(this.invalidInput).should('have.length', 4)
+    }
 }
 
 export default RegisterPage
