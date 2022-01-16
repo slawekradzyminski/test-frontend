83c272cc2c7798760a862b7aef8b3cec31513efd
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-01-16 16:50:53 +0100

    310

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ Application should run on [http://localhost:8080](http://localhost:8080)
 npm install -g allure-commandline
 allure serve
 ```
+
+# Overrido config
+
+```commandline
+npx cypress run --config-file conf.json --browser chrome --headed --spec 'cypress/integration/login.spec.js'
+```
--- cypress/integration/mocks/edit.mock.spec.js
+++ cypress/integration/mocks/edit.mock.spec.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import { getRandomString } from '../../util/random'
+
 describe('home page with mocks', () => {
     const users = require('../../fixtures/users.json')
 
@@ -7,16 +9,48 @@ describe('home page with mocks', () => {
         cy.setTokenInLocalStorage()
         cy.intercept('GET', '**/users', { fixture: 'users.json' })
         cy.visit('')
+        cy.intercept('GET', `**/users/${users[0].id}`, { fixture: 'firstUser.json' })
+        cy.get('ul li').contains(`${users[0].firstName} ${users[0].lastName}`).find('.edit').click()
     })
 
     it('should autofill user data', () => {
-        cy.intercept('GET', `**/users/${users[0].id}`, { fixture: 'firstUser.json' })
-        cy.get('ul li').contains(`${users[0].firstName} ${users[0].lastName}`).find('.edit').click()
-
         cy.get('[name=firstName]').should('have.value', users[0].firstName)
         cy.get('[name=lastName]').should('have.value', users[0].lastName)
         cy.get('[name=username]').should('have.value', users[0].username)
         cy.get('[name=password]').should('have.value', users[0].password)
+    })
+
+    it('should update user data', () => {
+        const newFirstName = getRandomString()
+        const newLastName = getRandomString()
+        const newUsername = getRandomString()
+        const newPassword = getRandomString()
+
+        cy.intercept('PUT', `**/users/${users[0].id}`, {
+            statusCode: 200,
+            body: {
+                id: users[0].id,
+                firstName: newFirstName,
+                lastName: newLastName,
+                username: newUsername,
+                password: newPassword
+            }
+        }).as('editRequest')
+
+        cy.get('[name=firstName]').clear().type(newFirstName)
+        cy.get('[name=lastName]').clear().type(newLastName)
+        cy.get('[name=username]').clear().type(newUsername)
+        cy.get('[name=password]').clear().type(newPassword)
+        cy.get('button').click()
+
+        cy.wait('@editRequest').its('request.body')
+            .should('deep.equal', {
+                id: users[0].id,
+                firstName: newFirstName,
+                lastName: newLastName,
+                username: newUsername,
+                password: newPassword
+            })
     })
 
 
