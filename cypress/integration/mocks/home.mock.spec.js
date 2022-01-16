10c9d12eb4c615dc286c624d7fc613930058fe36
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-01-16 15:37:23 +0100

    38

--- cypress/integration/mocks/home.mock.spec.js
+++ cypress/integration/mocks/home.mock.spec.js
@@ -15,4 +15,20 @@ describe('home page with mocks', () => {
         cy.get('ul li').eq(1).should('contain.text', `${users[1].firstName} ${users[1].lastName}`)
     })
 
+    it('should delete a user', () => {
+        cy.intercept('DELETE', `**/users/${users[0].id}`, { statusCode: 200 }).as('deleteRequest')
+        cy.get('ul li').contains(`${users[0].firstName} ${users[0].lastName}`).find('.delete').click()
+
+        cy.get('ul li').should('have.length', users.length - 1)
+        cy.get('ul li').contains(`${users[0].firstName} ${users[0].lastName}`).should('not.exist')
+        cy.wait('@deleteRequest')
+    })
+
+    it('should show Deleting... during delete', () => {
+        cy.intercept('DELETE', `**/users/${users[0].id}`, { delay: 10000 })
+        cy.get('ul li').contains(`${users[0].firstName} ${users[0].lastName}`).find('.delete').click()
+
+        cy.get('ul li').contains(`${users[0].firstName} ${users[0].lastName}`).should('contain.text', 'Deleting')
+    })
+
 })
