03df85e4ea709e7d3dbe6fe08ae3a11066d63985
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-02-13 10:37:14 +0100

    feb33

--- cypress/integration/isolation/login.stub.spec.js
+++ cypress/integration/isolation/login.stub.spec.js
@@ -28,4 +28,61 @@ describe('Login page in isolation', () => {
         cy.get('h1').should('contain.text', `Hi ${firstName}`)
     })
 
+    it('should fail to login', () => {
+        const message = 'You failed'
+
+        cy.intercept('POST', '**/users/authenticate', {
+            statusCode: 401,
+            body: {
+                message: message
+            }
+        })
+
+        cy.get('.form-control').eq(0).type('wrong')
+        cy.get('.form-control').eq(1).type('wrong')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert')
+            .should('have.text', message)
+            .should('have.class', 'alert-danger')
+    })
+
+    it('should handle server error', () => {
+        cy.intercept('POST', '**/users/authenticate', {
+            statusCode: 500
+        })
+
+        cy.get('.form-control').eq(0).type('wrong')
+        cy.get('.form-control').eq(1).type('wrong')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert')
+            .should('have.class', 'alert-danger')
+    })
+
+    it('should handle network disconnect', () => {
+        cy.intercept('POST', '**/users/authenticate', {
+            forceNetworkError: true
+        })
+
+        cy.get('.form-control').eq(0).type('wrong')
+        cy.get('.form-control').eq(1).type('wrong')
+        cy.get('.btn-primary').click()
+
+        cy.get('.alert')
+            .should('have.class', 'alert-danger')
+    })
+
+    it('should display spinner while waiting', () => {
+        cy.intercept('POST', '**/users/authenticate', {
+            delay: 3000
+        })
+
+        cy.get('.form-control').eq(0).type('wrong')
+        cy.get('.form-control').eq(1).type('wrong')
+        cy.get('.btn-primary').click()
+
+        cy.get('.btn-primary .spinner-border').should('be.visible')
+    })
+
 })
