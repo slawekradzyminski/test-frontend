9d4f85b6a806aab71dd98b3e912812c82197cf4f
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-03-19 13:41:16 +0100

    m34

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -6,15 +6,19 @@ describe('home page', () => {
         cy.login('slawenty', 'password')
     })
 
-    it('should display at least one user', () => { 
+    it('should display at least one user', () => {
         cy.get('ul li').should('have.length.at.least', 1)
     })
 
     it('should log user out', () => {
-        cy.get('#logout').click()    
+        cy.get('#logout').click()
         cy.url().should('contain', 'login')
         cy.get('h2').should('have.text', 'Login')
     })
 
-    
+    it("add more users", () => {
+        cy.get("#addmore").click();
+        cy.url().should("contain", "add-user");
+    });
+
 })
