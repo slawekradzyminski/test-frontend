1cac622f80c888a6b8636ab1a7ccf930277267f2
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-03-19 14:49:09 +0100

    m26

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,15 +1,34 @@
 /// <reference types="cypress" />
 
+import { getRandomString } from "../util/random"
+
 describe('login page', () => {
     beforeEach(() => {
         cy.visit('http://localhost:8080')
     })
 
     it('should successfully login', () => {
-        cy.get("input[name='username']").type('slawenty')
-        cy.get("input[name='password']").type('password')
+        const firstName = getRandomString()
+        const username = getRandomString()
+        const password = getRandomString()
+
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:4000/users/register',
+            body: {
+                firstName: firstName,
+                lastName: getRandomString(),
+                password: password,
+                username: username
+            }
+        }).then(resp => {
+            expect(resp.status).to.eq(201)
+        })
+
+        cy.get("input[name='username']").type(username)
+        cy.get("input[name='password']").type(password)
         cy.get('.btn-primary').click()
-        cy.get('h1').should('contain.text', 'Slawomir')
+        cy.get('h1').should('contain.text', firstName)
     })
 
     it('should fail to login', () => {
