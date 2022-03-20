a9afe83f9e38d837b10a0eb0827499ca92762f47
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-03-20 12:03:15 +0100

    m34

--- cypress/integration/mocks/register.mock.spec.js
+++ cypress/integration/mocks/register.mock.spec.js
@@ -10,25 +10,64 @@ describe('register page', () => {
     it('should successfully register', () => {
         const username = getRandomString()
         const password = getRandomString()
+        const firstName = getRandomString()
+        const lastName = getRandomString()
 
         cy.intercept('POST', '**/users/register', {
             statusCode: 201,
             body: {
-                firstName: getRandomString(),
+                firstName: firstName,
                 id: 1,
-                lastName: getRandomString(),
+                lastName: lastName,
                 password: password,
                 username: username
-                }
-        })
+            }
+        }).as('registerRequest')
 
-        cy.get('[name=firstName]').type(getRandomString())
-        cy.get('[name=lastName]').type(getRandomString())
+        cy.get('[name=firstName]').type(firstName)
+        cy.get('[name=lastName]').type(lastName)
         cy.get('[name=username]').type(username)
         cy.get('[name=password]').type(password)
         cy.get('.btn-primary').click()
         cy.get('.alert-success')
             .should('have.text', 'Registration successful')
+
+        cy.wait('@registerRequest').its('request.body').should('deep.equal', {
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            password: password
+        })
+    })
+
+    it('should fail to register', () => {
+        cy.intercept('POST', '**/users/register', {
+            statusCode: 400,
+            body: {
+                message: "User already exists"
+            }
+        })
+
+        cy.get('[name=firstName]').type(getRandomString())
+        cy.get('[name=lastName]').type(getRandomString())
+        cy.get('[name=username]').type(getRandomString())
+        cy.get('[name=password]').type(getRandomString())
+        cy.get('.btn-primary').click()
+        cy.get('.alert-danger')
+            .should('have.text', 'User already exists')
+    })
+
+    it('should show loading state', () => {
+        cy.intercept('POST', '**/users/register', {
+            delay: 2000
+        })
+
+        cy.get('[name=firstName]').type(getRandomString())
+        cy.get('[name=lastName]').type(getRandomString())
+        cy.get('[name=username]').type(getRandomString())
+        cy.get('[name=password]').type(getRandomString())
+        cy.get('.btn-primary').click()
+        cy.get('.spinner-border').should('be.visible');
     })
 
 })
